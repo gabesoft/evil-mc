fe1cc9416035e52cfd1ccc807cbc0bd4abb7fb7a
Gabriel Adomnicai <gabesoft@gmail.com>
2015-10-16 20:39:20 -0700

    Use a key map for defining key bindings. Fixes #5.

--- evil-mc.el
+++ evil-mc.el
@@ -53,19 +53,41 @@
 
 (put 'evil-mc-mode-line 'risky-local-variable t)
 
+(defvar evil-mc-key-map
+  (let ((map (make-sparse-keymap))
+        (keys '(("grm" . evil-mc-make-all-cursors)
+                ("gru" . evil-mc-undo-all-cursors)
+                ("grs" . evil-mc-pause-cursors)
+                ("grr" . evil-mc-resume-cursors)
+                ("grf" . evil-mc-make-and-goto-first-cursor)
+                ("grl" . evil-mc-make-and-goto-last-cursor)
+                ("grh" . evil-mc-make-cursor-here)
+                ("M-n" . evil-mc-make-and-goto-next-cursor)
+                ("grN" . evil-mc-skip-and-goto-next-cursor)
+                ("M-p" . evil-mc-make-and-goto-prev-cursor)
+                ("grP" . evil-mc-skip-and-goto-prev-cursor)
+                ("C-n" . evil-mc-make-and-goto-next-match)
+                ("grn" . evil-mc-skip-and-goto-next-match)
+                ("C-t" . evil-mc-skip-and-goto-next-match)
+                ("C-p" . evil-mc-make-and-goto-prev-match)
+                ("grp" . evil-mc-skip-and-goto-prev-match))))
+    (dolist (key-data keys)
+      (evil-define-key 'normal map (kbd (car key-data)) (cdr key-data))
+      (evil-define-key 'visual map (kbd (car key-data)) (cdr key-data)))
+    map))
+
 ;;;###autoload
 (define-minor-mode evil-mc-mode
   "Toggle evil multiple cursors in a single buffer."
   :group 'evil-mc
   :init-value nil
+  :keymap evil-mc-key-map
   :lighter evil-mc-mode-line
   (cond (evil-mc-mode
          (evil-mc-define-vars)
          (evil-mc-initialize-vars)
-         (evil-mc-initialize-keys)
          (evil-mc-initialize-hooks))
         (t
-         (evil-mc-teardown-keys)
          (evil-mc-teardown-hooks))))
 
 (put 'evil-mc-mode 'permanent-local t)
@@ -105,33 +127,7 @@
     "Custom command handlers. The entries here should have
 the same form as those in `evil-mc-known-commands'.
 This variable can be used to override default command handlers
-implementations.")
-
-  (defvar evil-mc-keys
-    '(("grm" . evil-mc-make-all-cursors)
-      ("gru" . evil-mc-undo-all-cursors)
-      ("grs" . evil-mc-pause-cursors)
-      ("grr" . evil-mc-resume-cursors)
-      ("grf" . evil-mc-make-and-goto-first-cursor)
-      ("grl" . evil-mc-make-and-goto-last-cursor)
-      ("grh" . evil-mc-make-cursor-here)
-      ("M-n" . evil-mc-make-and-goto-next-cursor)
-      (",N" . evil-mc-skip-and-goto-next-cursor)
-      ("grN" . evil-mc-skip-and-goto-next-cursor)
-      ("M-p" . evil-mc-make-and-goto-prev-cursor)
-      (",P" . evil-mc-skip-and-goto-prev-cursor)
-      ("grP" . evil-mc-skip-and-goto-prev-cursor)
-      ("C-n" . evil-mc-make-and-goto-next-match)
-      (",n" . evil-mc-skip-and-goto-next-match)
-      ("grn" . evil-mc-skip-and-goto-next-match)
-      ("C-t" . evil-mc-skip-and-goto-next-match)
-      ("C-p" . evil-mc-make-and-goto-prev-match)
-      (",p" . evil-mc-skip-and-goto-prev-match)
-      ("grp" . evil-mc-skip-and-goto-prev-match))
-    "Association list of key maps.
-Entries have the form (KEY . DEF), where KEY is the key
-that would trigger the `evil-mc' DEF.  The keys defined here
-will be set up in `normal' and `visual' mode."))
+implementations."))
 
 (defun evil-mc-initialize-vars ()
   "Initialize all variables used by `evil-mc'."
@@ -143,26 +139,6 @@ will be set up in `normal' and `visual' mode."))
   (evil-mc-clear-recording-debug)
   (evil-mc-clear-cursor-list)
   (evil-mc-resume-cursors))
-
-(defun evil-mc-define-key (map key def)
-  "In MAP define key sequence KEY as DEF."
-  (when (and (boundp map) (keymapp (symbol-value map)))
-    (define-key (symbol-value map) key def)))
-
-(defun evil-mc-initialize-keys ()
-  "Setup the `evil-mc' keys for normal and visual states."
-
-  (when (bound-and-true-p evil-mode)
-    (dolist (key evil-mc-keys)
-      (evil-mc-define-key 'evil-normal-state-local-map (kbd (car key)) (cdr key))
-      (evil-mc-define-key 'evil-visual-state-local-map (kbd (car key)) (cdr key)))))
-
-(defun evil-mc-teardown-keys ()
-  "Remove the `evil-mc' keys for normal and visual states."
-  (when (and (bound-and-true-p evil-mode) (boundp 'evil-mc-keys))
-    (dolist (key evil-mc-keys)
-      (evil-mc-define-key 'evil-normal-state-local-map (kbd (car key)) nil)
-      (evil-mc-define-key 'evil-visual-state-local-map (kbd (car key)) nil))))
 
 (defun evil-mc-pause-incompatible-modes ()
   "Temporarily disable incompatible minor modes."
@@ -192,24 +168,22 @@ will be set up in `normal' and `visual' mode."))
 
 (defun evil-mc-initialize-active-state ()
   "Initialize all variables and hooks used while there are active cursors."
-  (when (bound-and-true-p evil-mode)
-    (evil-mc-clear-command)
-    (evil-mc-clear-executing-command)
-    (evil-mc-clear-recording-command)
-    (add-hook 'pre-command-hook 'evil-mc-begin-command-save nil t)
-    (add-hook 'post-command-hook 'evil-mc-finish-command-save t t)
-    (add-hook 'post-command-hook 'evil-mc-execute-for-all t t)
-    (advice-add 'evil-repeat-keystrokes :before #'evil-mc-save-keys-motion)
-    (advice-add 'evil-repeat-motion :before #'evil-mc-save-keys-operator)))
+  (evil-mc-clear-command)
+  (evil-mc-clear-executing-command)
+  (evil-mc-clear-recording-command)
+  (add-hook 'pre-command-hook 'evil-mc-begin-command-save nil t)
+  (add-hook 'post-command-hook 'evil-mc-finish-command-save t t)
+  (add-hook 'post-command-hook 'evil-mc-execute-for-all t t)
+  (advice-add 'evil-repeat-keystrokes :before #'evil-mc-save-keys-motion)
+  (advice-add 'evil-repeat-motion :before #'evil-mc-save-keys-operator))
 
 (defun evil-mc-teardown-active-state ()
   "Teardown all variables and hooks used while there are active cursors."
-  (when (bound-and-true-p evil-mode)
-    (remove-hook 'pre-command-hook 'evil-mc-begin-command-save t)
-    (remove-hook 'post-command-hook 'evil-mc-finish-command-save t)
-    (remove-hook 'post-command-hook 'evil-mc-execute-for-all t)
-    (advice-remove 'evil-repeat-keystrokes #'evil-mc-save-keys-motion)
-    (advice-remove 'evil-repeat-motion #'evil-mc-save-keys-operator)))
+  (remove-hook 'pre-command-hook 'evil-mc-begin-command-save t)
+  (remove-hook 'post-command-hook 'evil-mc-finish-command-save t)
+  (remove-hook 'post-command-hook 'evil-mc-execute-for-all t)
+  (advice-remove 'evil-repeat-keystrokes #'evil-mc-save-keys-motion)
+  (advice-remove 'evil-repeat-motion #'evil-mc-save-keys-operator))
 
 (provide 'evil-mc)
 
